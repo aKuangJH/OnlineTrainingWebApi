2ee05f5df9d59feb3754a0850069eee4d68565f5
kuangjh <kuangjh@EBJ1200967.local>
2019-04-01 15:41:53 +0800

    修改首页，添加manage页面

--- js/common/common.js
+++ js/common/common.js
@@ -1,5 +1,5 @@
  
- // 获取Cookie值
+// 获取Cookie值
 function get_cookie(Name) {
    var search = Name + "="//查询检索的值
    var returnvalue = "";//返回值
@@ -17,3 +17,37 @@ function get_cookie(Name) {
    return returnvalue;
 }
 
+//设置cookie
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+
+//日期类型转换
+function parseTime(timestamp) {
+	var date = new Date(parseInt(timestamp)).toLocaleDateString();
+	//输出结果为2016/8/9
+	date = formatDate(date);
+	//输出结果为2016-08-09，满足YYYY-MM-DD格式要求
+	return date;
+}
+
+//日期格式转换
+function formatDate(date) {
+	dates = date.split("/");
+	if (dates.length == 3) {
+		if (dates[1].length == 1) {
+			dates[1] = "0" + dates[1];
+		}
+		if (dates[2].length == 1) {
+			dates[2] = "0" + dates[2];
+		}
+		date = dates.join("-");
+		return date;
+	} else {
+		return null;
+	}
+}
