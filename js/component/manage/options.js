9a3aa7f1dd123e2d195ffacff2c9c940b001fde9
ruier.dong <ruier.dong@ly.com>
2019-04-10 11:30:40 +0800

    实时检查注册信息，试题详情页

--- js/component/manage/options.js
+++ js/component/manage/options.js
@@ -3,7 +3,6 @@ $(function(){
 });
 
 function addnewquestion(){
-	let tid = GetQueryString("tid");
 	// alert("跳转新增问题页");
-	window.location.href = "http://train.online.com/html/manage/addquestion.html?tid="+tid;
+	window.location.href = "http://train.online.com/html/manage/addquestion.html";
 }
